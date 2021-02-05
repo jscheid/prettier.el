47bcd6ccb810e5b3de5aa1dee7588799a654e006
Julian Scheid <julians37@gmail.com>
2021-02-05 23:23:42 +1300

    Add test cases for ruby and php plugins

---
+++ test-cases/php-mode/a.prettier.php
@@ -0,0 +1,16 @@
+<?php
+
+array_map(
+    function ($arg1, $arg2) use ($var1, $var2) {
+        return $arg1 + $arg2 / ($var + $var2);
+    },
+    [
+        "complex" => "code",
+        "with" => function () {
+            return "inconsistent";
+        },
+        "formatting" => "is",
+        "hard" => "to",
+        "maintain" => true,
+    ]
+);
